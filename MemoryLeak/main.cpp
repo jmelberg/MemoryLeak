ab4561662327b7ce2afdab21b558736549751e3c
jmelberg <jordan.melberg@gmail.com>
2014-05-31 15:02:36 -0700

    Initial Commit

---
+++ MemoryLeak/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MemoryLeak
+//
+//  Created by Jordan Melberg on 5/31/14.
+//  Copyright (c) 2014 Jordan Melberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
